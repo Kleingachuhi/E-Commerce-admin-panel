42b03efe6eafbd24bcfe7ae3c966b8db61b50bb3
Klein <klein.gachuhi@student.moringaschool.com>
2025-04-22 11:56:25 +0300

    Add home page introduction and blank section styles

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className='home-page'>
-      
+    <>
+    <div className='home-page-introduction'>
+      <h2 className='introduction-h2' >Welcome to our seamless mode of management </h2>
     </div>
+    <section className='blank-section' ></section>
+    </>
   )
 }
 
